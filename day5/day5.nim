f7c02f988177764fc28c3439970fbbc30ce89be0
NotMichaelChen <mzc12345@gmail.com>
2020-12-08 19:38:43 -0500

    Less duplication in day5

--- day5/day5.nim
+++ day5/day5.nim
@@ -4,47 +4,28 @@ import sugar
 import strformat
 import algorithm
 
-func partOne(input: seq[string]): int =
-  return input
-    .map(proc(seatCode: string): int =
-      let rowNum = cast[string](
-          seatCode
-            .substr(0, 6)
-            .map(c => (if c == 'B': '1' else: '0'))
-        )
-        .fromBin[:int]()
+func seatCodeToSeatId(seatCode: string): int =
+  let rowNum = cast[string](
+    seatCode
+      .substr(0, 6)
+      .map(c => (if c == 'B': '1' else: '0'))
+  )
+  .fromBin[:int]()
       
-      let columnNum = cast[string](
-        seatCode
-          .substr(7)
-          .map(c => (if c == 'R': '1' else: '0'))
-      )
-      .fromBin[:int]()
+  let columnNum = cast[string](
+    seatCode
+      .substr(7)
+      .map(c => (if c == 'R': '1' else: '0'))
+  )
+  .fromBin[:int]()
 
-      return rowNum * 8 + columnNum
-    )
-    .max
+  return rowNum * 8 + columnNum
+
+func partOne(input: seq[string]): int =
+  return input.map(seatCodeToSeatId).max
 
 proc partTwo(input: seq[string]): int =
-  let sortedIds = input
-    .map(proc(seatCode: string): int =
-      let rowNum = cast[string](
-          seatCode
-            .substr(0, 6)
-            .map(c => (if c == 'B': '1' else: '0'))
-        )
-        .fromBin[:int]()
-      
-      let columnNum = cast[string](
-        seatCode
-          .substr(7)
-          .map(c => (if c == 'R': '1' else: '0'))
-      )
-      .fromBin[:int]()
-
-      return rowNum * 8 + columnNum
-    )
-    .sorted
+  let sortedIds = input.map(seatCodeToSeatId).sorted
   
   for i in countup(1, sortedIds.len - 2):
     if sortedIds[i] - sortedIds[i-1] != 1:
